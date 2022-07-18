8e75ab84676f39d901a1d4087b20fc9337785a4a
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-18 16:28:02 +0000

    新規登録機能追加しました。

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,33 @@
 class Admin::ItemsController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item)
+    @genres = Genre.all
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+    private
+    
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+  
 end
