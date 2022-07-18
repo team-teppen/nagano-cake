48f81043b74b751bd2a32674f65c9cfbe926321b
K-M0613 <yishumenma245@gmail.com>
2022-07-18 08:39:10 +0000

    マイページ作成、会員情報更新機能の修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
   }
-  
+
+  get 'customers/edit' => 'public/customers#edit', as: 'edit_customer'
+  patch 'customers' => 'public/customers#update', as: 'update_customer'
+
   devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -12,8 +15,8 @@ Rails.application.routes.draw do
   get "about" => "public/homes#about", as: "about"
 
   resources :items, only: [:index, :show]
-  resources :registrations, only: [:new, :create]
-  resource :customers, only: [:edit, :update]
+
+  
   get 'customers/unsubscribe' => 'public/customers#unsubscribe', as: 'unsubscribe'
   patch 'customers/withdraw' => 'public/customers#withdraw', as: 'withdraw'
   get 'customers/my_page' => 'public/customers#show', as: 'my_page'
