b108cf3c25bfb8a37e2f51756c1ebf34981331fe
K-M0613 <yishumenma245@gmail.com>
2022-07-19 11:05:40 +0000

    会員側商品一覧作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,20 @@
 class Public::ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.order(created_at: :desc).where(is_active: "販売可").page(params[:page]).per(8)
+    else
+      @items = Item.where(is_active: "販売可").page(params[:page]).per(12)
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :is_active, :genre_id)
+  end
+
 end
