689f750f54c4130b8026e56cb21b2e3a799b6d4f
sumio96k <106654738+sumio96k@users.noreply.github.com>
2022-07-19 03:19:00 +0000

    enum記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,15 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  
+
   validates :image, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :genre_id, presence: true
   validates :price, presence: true
-  
+
   has_many :cart_items, dependent: :destroy
+
+  has_many :order_details, dependent: :destroy
 
 
   def with_tax_price
