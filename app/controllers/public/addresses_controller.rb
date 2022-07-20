44ca925be3a4e0b20147bb22d6d70fa9b973ec25
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-20 09:58:48 +0000

    配送先登録一覧の作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,37 @@
 class Public::AddressesController < ApplicationController
+  
+    before_action :authenticate_customer!
+
+    
+    def index
+        @address = Address.new
+        @addresses = Address.all
+    end
+    
+    def edit
+      @address = Address.find(params[:id])
+    end
+    
+    def create
+      @address = Address.new(address_params)
+      @address.save
+      redirect_to addresses_path(@address)
+    end
+    
+    def update
+    end
+    
+    def destroy
+      @address = Address.find(params[:id])
+      @address.destroy
+      redirect_to addresses_path
+    end
+    
+    
+    private
+    
+    def address_params
+        params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+    end
+  
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
   
+  belongs_to :customer
   
 end
