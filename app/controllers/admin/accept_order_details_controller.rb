c898df137b091f4d1599c5cd2fce560fda1757b1
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-20 03:03:50 +0000

    注文履歴詳細作成途中

--- app/controllers/admin/accept_order_details_controller.rb
+++ app/controllers/admin/accept_order_details_controller.rb
@@ -1,2 +1,6 @@
 class Admin::AcceptOrderDetailsController < ApplicationController
+  
+  def update
+  end
+  
 end
--- app/controllers/admin/accept_orders_controller.rb
+++ app/controllers/admin/accept_orders_controller.rb
@@ -1,2 +1,16 @@
 class Admin::AcceptOrdersController < ApplicationController
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+  
+  def update
+  end
+  
+      private
+    
+  def order_params
+    params.require(:order).permit(:last_name, :first_name, :created_at, :address, :payment_method, :status)
+  end
+  
 end
