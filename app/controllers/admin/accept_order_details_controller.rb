f4a3f9fadb9cfc49c3ccc83d12d499c04789a93b
RyoyaMine <106651958+tyomine@users.noreply.github.com>
2022-07-22 12:04:05 +0000

    注文ステータスと製作ステータスの連動

--- app/controllers/admin/accept_order_details_controller.rb
+++ app/controllers/admin/accept_order_details_controller.rb
@@ -3,7 +3,16 @@ class Admin::AcceptOrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:id])
     @order_detail.update(order_detail_params)
-      redirect_to admin_accept_order_path(@order_detail.order.id)
+    # 製作ステータスのどれかが製作中になったら、注文ステータスも製作中になる
+    if params[:order_detail][:making_status] == "manufacturing"
+      Order.find_by(id: @order_detail.order_id).update(status: 2)
+    elsif params[:order_detail][:making_status] == "finish"
+    # 全て製作完了になったら注文ステータスが発送準備中に変わる
+      if OrderDetail.where(order_id: @order_detail.order_id).where.not(making_status: 3).exists? != true
+        Order.find_by(id: @order_detail.order_id).update(status: 3)
+      end
+    end
+    redirect_to admin_accept_order_path(@order_detail.order.id)
   end
   
   
